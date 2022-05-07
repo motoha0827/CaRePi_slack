80b5caa032dada190420438efeeedb2551c2e166
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-05-07 14:32:32 +0900

    update: app.py

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import os
 from dotenv import load_dotenv
-
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-
 import datetime
+import requests
 
 load_dotenv()
 
 app = App(token=os.environ['SLACK_BOT_TOKEN'])
+
+@app.command("/carepi")
+def handle_carepi_command(ack, say, command):
+    ack()
+    name = command["user_name"]
+    sn = name[:1] + "11140" + name[3:6]
+    print(sn)
 
 if __name__ == '__main__':
   SocketModeHandler(app, os.environ['SLACK_APP_TOKEN']).start()
