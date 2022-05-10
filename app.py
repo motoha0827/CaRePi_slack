11e64f2b098859a1c18fe9f904e3e5de8b9ab779
Akimoto <82797330+motoha0827@users.noreply.github.com>
2022-05-10 17:34:40 +0900

    update: refactor

--- app.py
+++ app.py
@@ -13,17 +13,16 @@ def handle_carepi_command(ack, say, command):
     ack()
     user_id = command["user_id"]
     channel_id = command["channel_id"]
-    Ephemeral_text = '【ERROR】\n表示名を 学籍番号 + 名字 の形式にしてください。(例:2011140062秋本)'
-
     display_name = app.client.users_info(user=user_id)['user']['profile']['display_name']
+    ephemeral_text = '【ERROR】\n表示名を 学籍番号 + 名字 の形式にしてください。(例:2011140062秋本)'
 
     check_name = re.match(r'[0-9]{10}.*', display_name)
     if check_name != None:
       student_num = display_name[:10] 
-      response = requests.post(f'http://localhost:3000/session?student_number={student_num}')
+      response = requests.post('http://localhost:3000/session', data = {'student_number': student_num})
       say(f'{response.json()["data"]} \n {command["text"]}')
     else:
-      app.client.chat_postEphemeral(channel=channel_id, text=Ephemeral_text, user=user_id)
+      app.client.chat_postEphemeral(channel=channel_id, text=ephemeral_text, user=user_id)
 
 if __name__ == '__main__':
   SocketModeHandler(app, os.environ['SLACK_APP_TOKEN']).start()
